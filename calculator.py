0136d2d981cd960a5a35a86e7e65f76690f3a704
Anju Kumari <101560181+98Anju@users.noreply.github.com>
2022-08-01 21:02:19 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    c=a+b
+    return c
+def sub(s,g):
+    d=s-add(10,5)
+    return d
+def mult(m,h):
+    e=m*h
+    return e
+def div(v,y):
+    k=v%y
+    return k
+def main(a,b):
+    return mult(5,6)
+j=main(5,6)
+print(j)
